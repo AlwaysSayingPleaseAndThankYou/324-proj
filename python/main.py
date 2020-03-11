2d1fec6c2cd852127aaf073680a6ff835eb238ea
Gus <aengus.crowley@gmail.com>
2020-03-11 01:53:11 -0700

    writting neopixel code

--- python/main.py
+++ python/main.py
@@ -1,1 +1,19 @@
-from Neopixel import neopixel
+from neopixel import Neopixel
+from machine import Pin
+from random import choice
+from time import sleep
+
+np = Neopixel(Pin(4), 8)
+
+
+def val_gen():
+  yield choice(range(255))
+
+
+def color_gen():
+  yield [next(val_gen()) for i in range(3)]
+
+
+while True:
+  np[choice(range(8))]=(next(color_gen()))
+  sleep(0.5)
